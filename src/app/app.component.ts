e0a743c89b2ba764ac8c0cb1c6820821c52c6a13
diegomarceloledezma <diegomarceloledezma@gmail.com>
2024-09-20 20:30:57 -0500

    [20-09-2024] interface and arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface IPerson {
+  name: string
+  lastName: string
+  age?: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,9 +16,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title: number = 10;
+  animals:string[]= ['a','b','c','d','e','f','g']
+
+  person: IPerson = {
+    name: 'a',
+    lastName: 'b',
+    // age: 5
+  }
 
   constructor() {
     console.log('substract', this.substract(8,4))
+
+    console.log('MAP: ', this.animals.map( (animal) => (animal + 'new') ))
+    console.log('FOREACH: ', this.animals.forEach( (animal) => (animal + 'new') ))
+    console.log('FIND: ', this.animals.find( (animal) => animal === 'd' ))
+    console.log('FILTER: ', this.animals.filter( (animal) => animal === 'e' ))
+    console.log('INDEXOF: ', this.animals.indexOf('c'))
+
   }
   public sum(num1: number, num2: number):number {
     return num1 + num2
@@ -29,4 +49,19 @@ export class AppComponent {
         console.log('person =', persons[i])
     }
   }
+
+  // function sumar(){
+  //   return 1+2;
+  // }
+
+  // arrow functions
+  // const suma = () => {
+  //   return 1+2
+  // }
+
+  // function resta (a){
+  //   return 'hola' + a;
+  // }
+
+  // const resta = (a) => ('hola' + a)
 }
