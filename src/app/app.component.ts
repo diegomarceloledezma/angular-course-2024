074ef0f7ec3d542a3e60f74063723216411d4bfc
diegomarceloledezma <diegomarceloledezma@gmail.com>
2024-09-20 19:49:46 -0500

    [20-09-2024] print only even numbers of an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,4 +21,12 @@ export class AppComponent {
   private substract(num1: number, num2: number):number{
     return num1 - num2
   }
+
+  public getArray(){
+    const persons:number[] = [1,2,3,4,5,6,7,8,9,10,11]
+    for(let i = 0; i < persons.length; i++){
+      if(persons[i]%2==0)
+        console.log('person =', persons[i])
+    }
+  }
 }
