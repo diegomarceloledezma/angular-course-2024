07e86ef91246bcf06f7211e70d891f8db0cc2427
Diego Ledezma <diegomarceloledezma@gmail.com>
2024-10-01 19:27:56 -0500

    [01-10-2024] DoCheck, AfterContentInit

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AfterContentInit, Component, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'user-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
 })
-export class UserCardComponent implements OnInit, OnDestroy, OnChanges {
+export class UserCardComponent implements OnInit, OnDestroy, OnChanges, DoCheck, AfterContentInit {
 
   @Input() name:string = ''
   @Input() email:string = ''
@@ -31,10 +32,20 @@ export class UserCardComponent implements OnInit, OnDestroy, OnChanges {
     console.log('user card destroy')
   }
 
+  //PARA CUANDO CAMBIA ALGO EN ALGUN INPUT
   ngOnChanges(changes: SimpleChanges): void {
     console.log('CHANGES:', changes)
 
     this.password = changes['name'].currentValue + ' ' + changes['email'].currentValue + 'PASSWORD'
+  }
+
+  //CUANDO HAY CAMBIOS EN GENERAL
+  ngDoCheck(): void {
+    console.log('DO CHECK user card')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('NG AFTER CONTENT INIT')
   }
 
   public onSendData(){
