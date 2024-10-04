fceb9bc649a821990300efebfd00cca3b299ce9c
Diego Ledezma <diegomarceloledezma@gmail.com>
2024-10-03 20:25:43 -0500

    [09-10-2024] phase write and read, attributes directives ngClass

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -13,28 +13,37 @@ export class CounterComponent {
 
   constructor() {
     afterRender({
-      earlyRead: () => {
-        console.log('INTO EARLY READ ')
-        const currentAppColor = this.appBackground;
-        return 'From earlyRead: ' + currentAppColor;
-      },
-      mixedReadWrite: (props) => {
-        console.log('INTO mixedReadWrite ', props)
-        if (props.indexOf('red') > -1) {
-          this.appBackground = 'green';
+      // earlyRead: () => {
+      //   console.log('INTO EARLY READ ')
+      //   const currentAppColor = this.appBackground;
+      //   return 'From earlyRead: ' + currentAppColor;
+      // },
+      // mixedReadWrite: (props) => {
+      //   console.log('INTO mixedReadWrite ', props)
+      //   if (props.indexOf('red') > -1) {
+      //     this.appBackground = 'green';
+      //   } else {
+      //     this.appBackground = 'red';
+      //   }
+      //   return 'From mixedReadWrite: ' + this.appBackground;
+      // },
+      write: () => {
+        console.log('INTO write ');
+        document.body.style.backgroundColor = this.appBackground;
+
+        const currentColor = this.appBackground;
+        if (currentColor.includes('red')) {
+          this.appBackground = 'blue';
         } else {
           this.appBackground = 'red';
         }
-        return 'From mixedReadWrite: ' + this.appBackground;
-      },
-      write: (props) => {
-        console.log('INTO write ', props)
-        document.body.style.backgroundColor = this.appBackground;
+
         return 'From write: ' + this.appBackground;
       },
       read: (props) => {
-        console.log('INTO READ ', props)
+        console.log('INTO READ ', props);
         const newBackground = this.appBackground;
+        console.log('From read: ', newBackground)
       },
     });
 
