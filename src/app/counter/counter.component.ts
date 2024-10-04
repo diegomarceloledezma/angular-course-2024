c5cc56a6581990fc963b540493e42304f0294494
Diego Ledezma <diegomarceloledezma@gmail.com>
2024-10-03 19:40:28 -0500

    [03-10-2024] phases in afterRender and afterNext

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -5,19 +5,41 @@ import { Component, afterRender, afterNextRender } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.scss'
+  styleUrl: './counter.component.scss',
 })
 export class CounterComponent {
-  sum:number = 0
+  sum: number = 0;
+  appBackground: string = 'red';
 
-  constructor(){
-    afterRender(() => {
-      console.log('AFTER RENDER: ', this.sum)
-    })
+  constructor() {
+    afterRender({
+      earlyRead: () => {
+        console.log('INTO EARLY READ ')
+        const currentAppColor = this.appBackground;
+        return 'From earlyRead: ' + currentAppColor;
+      },
+      mixedReadWrite: (props) => {
+        console.log('INTO mixedReadWrite ', props)
+        if (props.indexOf('red') > -1) {
+          this.appBackground = 'green';
+        } else {
+          this.appBackground = 'red';
+        }
+        return 'From mixedReadWrite: ' + this.appBackground;
+      },
+      write: (props) => {
+        console.log('INTO write ', props)
+        document.body.style.backgroundColor = this.appBackground;
+        return 'From write: ' + this.appBackground;
+      },
+      read: (props) => {
+        console.log('INTO READ ', props)
+        const newBackground = this.appBackground;
+      },
+    });
 
     afterNextRender(() => {
-      console.log('AFTER NEXT RENDER: ', this.sum)
-    })
+      console.log('AFTER NEXT RENDER: ', this.sum);
+    });
   }
-
 }
