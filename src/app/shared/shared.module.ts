0ac7cd7c865ce165e1b11c760c728c5cd961f905
Diego Ledezma <diegomarceloledezma@gmail.com>
2024-10-07 19:57:34 -0500

    [07-10-2024]Style and class syntax and ngModule example

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,6 +10,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   declarations: [AlertComponent, CardComponent, NavbarComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [AlertComponent, CardComponent, NavbarComponent],
 })
-export class SharedModuleModule { }
+export class SharedModule {  }
--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -17,11 +17,12 @@ import {
   AfterViewChecked,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'user-card',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, SharedModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
 })
