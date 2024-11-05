ac2165caff2299198474267e1a970f1ade9d2d07
Diego Ledezma <diegomarceloledezma@gmail.com>
2024-11-04 18:16:37 -0600

    [04-11-2024] Auth Interceptor

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  private token!: string
+  private token!: string | null
   private user!: string
 
 
@@ -14,16 +14,23 @@ export class AuthService {
   public login(){
     this.token = '123ABC'
     this.user = 'test@gmail.com'
+    localStorage.setItem('token', this.token)
     return this.token
   }
 
   public logout(){
     this.token = ''
     this.user = ''
+    localStorage.removeItem('token')
   }
 
   public getUser(){
     return this.user
   }
 
+  public getToken(){
+    const token = localStorage.getItem('token')
+    this.token = token ?? null
+    return this.token
+  }
 }
